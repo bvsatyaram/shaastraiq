0b809a276199e344af6e93c42a87f527802dce94
B V Satyaram <bvsatyaram@gmail.com>
2010-09-11 22:08:50 +0530

    Renaming vol admin user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   acts_as_authentic
 
   def admin?
-    manageable_users = ["bvsatyaram", "towel", "pratul", "iq_vol", "angel"]
+    manageable_users = ["bvsatyaram", "towel", "pratul", "iqvol", "angel"]
     manageable_users.include?(self.username)
   end
 end
