aa2ed67ded35c5bfea21e3bdd93330bb98d5554d
B V Satyaram <bvsatyaram@gmail.com>
2010-09-12 11:38:55 +0530

    Changing admins

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   acts_as_authentic
 
   def admin?
-    manageable_users = ["bvsatyaram", "towel", "pratul", "iqvol", "angel"]
+    manageable_users = ["bvsatyaram", "iqcoord", "iqvol", "angel"]
     manageable_users.include?(self.username)
   end
 end
